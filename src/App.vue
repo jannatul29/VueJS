39c3d10a4201a4b2d992584dc6038b54b58b8743
jannatul29 <2017-2-60-029@std.ewubd.bd>
2022-01-06 14:16:22 +0600

    complete project full

--- src/App.vue
+++ src/App.vue
@@ -596,7 +596,7 @@
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                 <h4 class="modal-title">Modal Heading</h4>
+                 <h4 class="modal-title">Select date</h4>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
               </div>
               <div class="modal-body">
